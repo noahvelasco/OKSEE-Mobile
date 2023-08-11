9af89fdb98f701549f62c0a99b93a1d48335259c
Noah Velasco <noahvelasco299@gmail.com>
2023-08-11 05:05:16 -0500

    shortened location descriptions

--- src/utils/locations.js
+++ src/utils/locations.js
@@ -4,7 +4,7 @@ export const locations = [
   {
     category: "pizza",
     title: "Sauced on Paseo",
-    description: "2912 Paseo, Oklahoma City, OK 73103",
+    description: "2912 Paseo, OKC",
     coordinate: {
       latitude: 35.49891757672362,
       longitude: -97.52166760519061,
@@ -13,7 +13,7 @@ export const locations = [
   {
     category: "pizza",
     title: "The Hall's Pizza Kitchen",
-    description: "1004 N Hudson Ave STE 106, Oklahoma City, OK 73102",
+    description: "1004 N Hudson Ave STE 106, OKC",
     coordinate: {
       latitude: 35.47754000215201,
       longitude: -97.51831896740379,
@@ -22,7 +22,7 @@ export const locations = [
   {
     category: "burger",
     title: "Nic's Grill",
-    description: "1201 N Pennsylvania Ave, Oklahoma City, OK 73107",
+    description: "1201 N Pennsylvania Ave, OKC",
     coordinate: {
       latitude: 35.48093461339461,
       longitude: -97.54800287178823,
@@ -31,7 +31,7 @@ export const locations = [
   {
     category: "burger",
     title: "Tucker's Onion Burger",
-    description: "324 NW 23rd St, Oklahoma City, OK 73118",
+    description: "324 NW 23rd St, OKC",
     coordinate: {
       latitude: 35.49299217006498,
       longitude: -97.51855314077812,
@@ -40,7 +40,7 @@ export const locations = [
   {
     category: "coffee",
     title: "Harvey Bakery & Kitchen",
-    description: "301 NW 13th St Suite 100, Oklahoma City, OK 73103",
+    description: "301 NW 13th St Suite 100, OKC",
     coordinate: {
       latitude: 35.48254697504061,
       longitude: -97.51760374444109,
@@ -49,7 +49,7 @@ export const locations = [
   {
     category: "coffee",
     title: "Coffee Slingers Roasters",
-    description: "1015 N Broadway Ave, Oklahoma City, OK 73102",
+    description: "1015 N Broadway Ave, OKC",
     coordinate: {
       latitude: 35.477695960395025,
       longitude: -97.51430486383741,
