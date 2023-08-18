56416c9677460f0a48fbda41fd2ca09d1faba351
Noah Velasco <noahvelasco299@gmail.com>
2023-08-18 01:56:20 -0500

    added border and icon to textbox

--- src/screens/locationsScreen/components/searchBar.js
+++ src/screens/locationsScreen/components/searchBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
 const SearchBar = ({ value, onChangeText }) => {
   return (
@@ -10,8 +11,14 @@ const SearchBar = ({ value, onChangeText }) => {
         style={styles.input}
         onChangeText={onChangeText}
         value={value}
-        placeholder="Hall's Pizzeria"
-        placeholderTextColor={"white"}
+        placeholder="Search..."
+        placeholderTextColor={"grey"}
+      />
+      <Entypo
+        name="magnifying-glass"
+        size={20}
+        color="white"
+        style={styles.icon}
       />
     </View>
   );
@@ -19,17 +26,26 @@ const SearchBar = ({ value, onChangeText }) => {
 
 const styles = StyleSheet.create({
   searchBar: {
+    flexDirection: "row",
+    alignSelf: "center",
     alignItems: "center",
-  },
-
-  input: {
-    height: 40,
-    width: "85%",
+    backgroundColor: "#333333",
     borderWidth: 2,
     borderRadius: 20,
     borderColor: "#C0A080",
-    backgroundColor: "#333333",
-    padding: 10,
+    width: "85%",
+    padding: 2,
+    paddingLeft: 10,
+  },
+  input: {
+    flex: 1,
+    color: "white",
+    height: 40,
+    padding: 0, // Reset the padding here and manage it in the searchBar wrapper
+  },
+  icon: {
+    position: "absolute",
+    right: 15, // Adjust according to your design
   },
 });
 
